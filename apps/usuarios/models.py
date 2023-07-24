6da032b5b2c436c046785e8f4dd3859ac6da6de5
Fwer <fenixo846@gmail.com>
2023-07-24 13:52:20 -0300

    agregado el campo imagen

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    pass
+    imagen = models.ImageField(null=True, blank=True, upload_to='usuarios', default='usuarios/user-default.png')
+
+    def get_absolute_url(self):
+        return reverse('index')
